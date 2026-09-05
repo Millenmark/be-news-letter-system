87077d478cdea63ae6eb188043944c7e599c9698
Millenmark <millenmark.dev@gmail.com>
2026-09-05 19:02:13 +0800

    change code order

--- tests/test_health.rs
+++ tests/test_health.rs
@@ -1,5 +1,13 @@
 use std::net::TcpListener;
 
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
+    let port = listener.local_addr().unwrap().port();
+    let server = news_letter::run(listener).expect("Failed to bind address.");
+    let _ = tokio::spawn(server);
+    format!("http://localhost:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
@@ -14,12 +22,4 @@ async fn health_check_works() {
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
-    let port = listener.local_addr().unwrap().port();
-    let server = news_letter::run(listener).expect("Failed to bind address.");
-    let _ = tokio::spawn(server);
-    format!("http://localhost:{}", port)
 }
