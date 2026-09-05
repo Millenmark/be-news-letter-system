6cddee777cc5c735d772af29440b5b87e3c671d8
Millenmark <millenmark.dev@gmail.com>
2026-09-05 17:07:50 +0800

    fix main app host

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0")?;
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
     run(listener)?.await
 }
