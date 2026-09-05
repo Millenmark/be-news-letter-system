8c3898a6fee36b27044f0226668e370fa4b0e1ba
Millenmark <millenmark.dev@gmail.com>
2026-09-05 14:19:36 +0800

    make the test more robust

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-// use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
-
-// async fn greet(req: HttpRequest) -> impl Responder {
-//     let name = req.match_info().get("name").unwrap_or("World");
-//     format!("Hello, {}!", name)
-// }
-
-// async fn health_check() -> impl Responder {
-//     HttpResponse::Ok()
-// }
-
 use be_email_news_letter::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    run("127.0.0.1:0")?.await
 }
