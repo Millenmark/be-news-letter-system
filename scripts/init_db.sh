be93a25debd2d78f70b952d770429a9fa0b27474
Millenmark <millenmark.dev@gmail.com>
2026-09-09 15:39:44 +0800

    create migration file

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,6 +1,14 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(command -v sqlx)" ]; then
+    echo >&2 "Error: sqlx is not installed"
+    echo >&2 "Use:"
+    echo >&2 "  cargo install sqlx-cli --no-default-features --features postgres"
+    echo >&2 "to install it"
+    exit 1
+fi
+
 DB_PORT="${POSTGRES_PORT:=5433}"
 SUPERUSER="${SUPERUSER:=postgres}"
 SUPERUSER_PWD="${SUPERUSER_PWD:=password}"
@@ -35,3 +43,8 @@ docker exec -it ${CONTAINER_NAME} psql -U ${SUPERUSER} -c "${CREATE_QUERY}"
 
 GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
 docker exec -it ${CONTAINER_NAME} psql -U ${SUPERUSER} -c "${GRANT_QUERY}"
+
+DATABASE_URL=postgres://${APP_USER}:${APP_USER_PWD}@localhost:${DB_PORT}/${APP_DB_NAME}
+export DATABASE_URL
+sqlx database create
+sqlx migrate add create_subscriptions_table
