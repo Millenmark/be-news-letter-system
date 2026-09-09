7e9a39bb52235499b952e614861501bc012fbea1
Millenmark <millenmark.dev@gmail.com>
2026-09-09 11:26:25 +0800

    create bash script to start the postgres docker image

---
+++ scripts/init_db.sh
@@ -0,0 +1,15 @@
+set -x
+set -eo pipefail
+
+DB_PORT="${POSTGRES_PORT:=5433}"
+SUPERUSER="${SUPERUSER:=postgres}"
+SUPERUSER_PWD="${SUPERUSER_PWD:=password}"
+
+CONTAINER_NAME="postgres-2"
+docker run \
+    --env POSTGRES_USER="${SUPERUSER}" \
+    --env POSTGRES_PASSWORD="${SUPERUSER_PWD}" \
+    --publish "${DB_PORT}:5432" \
+    --detach \
+    --name "${CONTAINER_NAME}" \
+    postgres -N 1000
