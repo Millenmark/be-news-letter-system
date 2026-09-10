b64efb70542ef5b404763e539c52cad658c6c117
Millenmark <millenmark.dev@gmail.com>
2026-09-10 16:11:40 +0800

    modularize the logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,3 @@
-use actix_web::{App, HttpResponse, HttpServer, dev::Server, web};
-use std::net::TcpListener;
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-#[derive(serde::Deserialize)]
-pub struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health", web::get().to(health_check))
-            .route("/subscribe", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-    Ok(server)
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
