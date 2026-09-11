898fc1950d353889c0073a50df361e759643ac62
Millenmark <millenmark.dev@gmail.com>
2026-09-11 17:12:31 +0800

    add connection to db

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use news_letter::configuration::get_configuration;
 use news_letter::startup::run;
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to database.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
